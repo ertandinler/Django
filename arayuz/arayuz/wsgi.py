d025804a3c3f93db864af831b22890627e4b54ef
ertandinler <ertandinler@yandex.com>
2014-05-17 03:17:29 +0300

    son

---
+++ arayuz/arayuz/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for arayuz project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "arayuz.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
